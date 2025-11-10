fbd8ddaacff055c63ade0f7af4fe51ab37b51061
siva946 <123248824+siva946@users.noreply.github.com>
2025-11-10 17:48:41 +0530

    Update CORS settings to restrict origins and enable credentials

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["https://resumsync.vercel.app/", "http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"]
