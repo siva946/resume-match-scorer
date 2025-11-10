a5748568bcd629f7c5897664f050ef1a1e4af8aa
siva946 <123248824+siva946@users.noreply.github.com>
2025-11-10 18:01:51 +0530

    Update CORS settings to include additional allowed origin

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://resumsync.vercel.app/", "http://localhost:3000"],
+    allow_origins=["https://resumsync-frontend.onrender.com","https://resumsync.vercel.app/", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
